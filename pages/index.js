85a94ae8b427b73b70085eccfe50352bb4576f4b
tatecarson <tatecarson@gmail.com>
2017-10-07 00:44:09 -0500

    mating pool working between different clients

--- pages/index.js
+++ pages/index.js
@@ -31,36 +31,21 @@ $(function() {
       population.population.loop.stop();
 
       //guard against undefined during performance
-      if (typeof population.population.dna.genes === "undefined") {
-        let geneList = [];
-
-        for (let i = 0; i < 20; i++) {
-          geneList[i] = _.random(0.0, 1.0, true);
-        }
-        population.population.dna.genes = geneList;
-      }
+      ifUndefined();
 
       //create next generation
-      //Add fitness and ID to an array so they can be concated with the genes array
       const fitID = [population.population.getFitness(), client.id];
       const data = fitID.concat(population.population.dna.genes);
       client.send("/fitness", data);
 
-      //clear fitness
-      nextGen();
+      population.selection();
+      population.reproduction();
       population.population.clear();
     }
   });
 });
 
-// preSeed();
-
-function nextGen() {
-  population.selection();
-  population.reproduction();
-}
-
-//rhizome testing
+//Rhizome code
 client.start(function(err) {
   if (err) throw err;
   console.log("subscribing...");
@@ -72,20 +57,11 @@ client.on("connected", function() {
 });
 
 client.on("message", function(addr, args) {
-  //FIXME: replace same client id instead of not adding
   if (addr === "/fitness") {
-    population.someOtherPopulation.forEach(pop => {
-      console.log("client id", pop.clientID);
-
-      if (pop.clientID !== client.id) {
-        console.log("the client id is the same: ");
-        //TODO: rename this to population because all client devices make up population
-        population.someOtherPopulation.push({
-          fitness: args[0],
-          clientID: args[1],
-          genes: args.slice(2, args.length) //turn back into an array
-        });
-      }
+    population.someOtherPopulation.push({
+      fitness: args[0],
+      clientID: args[1],
+      genes: args.slice(2, args.length) //turn back into an array
     });
   }
 });
@@ -109,8 +85,20 @@ function preSeed() {
       clientID: client.id,
       genes: geneList //turn back into an array
     });
-  } //seed object at startfor before client gets data from other clients
+  }
 }
+
+function ifUndefined() {
+  if (typeof population.population.dna.genes === "undefined") {
+    let geneList = [];
+
+    for (let i = 0; i < 20; i++) {
+      geneList[i] = _.random(0.0, 1.0, true);
+    }
+    population.population.dna.genes = geneList;
+  }
+}
+
 Tone.Transport.start("+0.1");
 Tone.Transport.bpm.value = _.random(100, 200);
 
