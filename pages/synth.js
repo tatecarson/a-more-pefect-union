e375e5d2ee5d8a9259877350fe8c2f4db8497475
tatecarson <tatecarson@gmail.com>
2017-10-16 17:19:23 -0500

    added some samples

--- pages/synth.js
+++ pages/synth.js
@@ -189,25 +189,62 @@ const treeTrunk = new Tone.Synth({
 }).toMaster();
 
 //TODO: experiment with building instruments
-var metal = new Tone.Players({
-  metal1: "./samples/bang_wood_on_metal1.wav",
-  metal2: "./samples/bang_wood_on_metal2.wav",
-  metal3: "./samples/bang_wood_on_metal3.wav",
-  metal4: "./samples/bang_wood_on_metal4.wav"
+const metal = new Tone.Players({
+  metal1: "./samples/metal/bang_wood_on_metal1.mp3",
+  metal2: "./samples/metal/bang_wood_on_metal2.mp3",
+  metal3: "./samples/metal/bang_wood_on_metal3.mp3",
+  metal4: "./samples/metal/bang_wood_on_metal4.mp3"
 }).toMaster();
 
+const bell = new Tone.Players({
+  bell1: "./samples/bell/bell_1a.mp3",
+  bell3: "./samples/bell/bell-octave0.mp3",
+  bell4: "./samples/bell/bell-octave1.mp3",
+  bell5: "./samples/bell/bell-octave2.mp3",
+  bell6: "./samples/bell/bellF.mp3"
+}).toMaster();
+
+const marimba = new Tone.Players({
+  marimbaHard4: "./samples/marimba/MarimbaHardC4.mp3",
+  marimbaHard5: "./samples/marimba/MarimbaHardC5.mp3",
+  marimbaHard6: "./samples/marimba/MarimbaHardC6.mp3",
+  marimbaSoft4: "./samples/marimba/MarimbaSoftC4.mp3",
+  marimbaSoft5: "./samples/marimba/MarimbaSoftC5.mp3",
+  marimbaSoft6: "./samples/marimba/MarimbaSoftC6.mp3"
+}).toMaster();
 //more instruments
 
+const kenong = new Tone.Players({
+  kenong1: "./samples/kenong/Kenong90 2-Audio.mp3",
+  kenong2: "./samples/kenong/Kenong90 3-Audio.mp3",
+  kenong3: "./samples/kenong/Kenong90 4-Audio.mp3",
+  kenong4: "./samples/kenong/Kenong90 5-Audio.mp3",
+  kenong5: "./samples/kenong/Kenong90 6-Audio.mp3"
+}).toMaster();
+
+const musicBox = new Tone.Players({
+  musicBox1: "./samples/music_box/music_box 3-Audio.mp3",
+  musicBox2: "./samples/music_box/music_box 4-Audio.mp3",
+  musicBox3: "./samples/music_box/music_box 5-Audio.mp3"
+}).toMaster();
+
+const panPot = new Tone.Players({
+  panPot1: "./samples/pan_pot/pan_pot 3-Audio.mp3",
+  panPot2: "./samples/pan_pot/pan_pot 4-Audio.mp3",
+  panPot3: "./samples/pan_pot/pan_pot 5-Audio.mp3"
+}).toMaster();
+
 var loop = new Tone.Loop(function(time) {
-  generatePlayer(metal);
+  generatePlayer(panPot);
 }, "8n");
 
 const generatePlayer = player => {
   let randSample = _.sample(Object.keys(player._players));
-  console.log("sample: ", randSample);
 
   player.get(randSample).start();
   player.get(randSample).playbackRate = Nexus.tune.ratio(_.random(0, 12));
 };
+
+Tone.Buffer.on("load", () => loop.start());
 
 // loop.start();
